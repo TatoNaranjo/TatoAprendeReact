addc4d6de0b7ac569e4496d0730bf7dee6af345a
TatoNaranjo <naranjosa2004@gmail.com>
2023-07-18 20:10:04 -0500

    Proyecto Administración de Pacientes

---
+++ citas-react/src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import Header from './components/Header'
+import Formulario from './components/Formulario'
+
+function App() {
+ 
+
+  return (
+    
+      <div className='App'>
+      <Header/>
+      <Formulario/>
+      </div>
+    
+  )
+}
+
+export default App
---
+++ citas-react/src/components/Formulario.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Formulario() {
+  return (
+    <h2>Formulario</h2>
+  )
+}
+
+export default Formulario
